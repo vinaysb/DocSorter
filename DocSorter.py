84d0ce003c3ed98a6f8141ac695b42cfeeece050
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-02-15 11:43:17 +0100

    Made the code more interactable

--- DocSorter.py
+++ DocSorter.py
@@ -4,6 +4,7 @@ import shutil
 
 def GetFiles(extension):
     import glob
+
     file_list = [j for j in glob.glob('*.{}'.format(extension))]
     return(file_list)
 
@@ -11,6 +12,7 @@ def GetFiles(extension):
 def docx_opr(file_name):
     import docx2txt
     from profanityfilter import ProfanityFilter
+
     pf = ProfanityFilter()
     document = docx2txt.process(file_name)
     words = document.split()
@@ -18,7 +20,13 @@ def docx_opr(file_name):
     return(len(words), profane)
 
 
+try:
+    max_len = int(input('Enter the maxumum number of allowed words'))
+except ValueError:
+    print('Please enter only numeric values')
+
 folders = [name for name in os.listdir(".") if os.path.isdir(name)]
+
 for folder in folders:
     # print(folder)
     os.chdir(folder)
@@ -28,8 +36,9 @@ for folder in folders:
         pass
     file_list = GetFiles('docx')
     for file in file_list:
-        # print(file)
+        print(file, 'Processed')
         wordlen, profane = docx_opr(file)
-        if (wordlen > 300) or not profane:
+        if (wordlen > max_len) or not profane:
             shutil.move(file, 'Disqualified/' + file)
     os.chdir("..")
+input('Press enter to close the window')
